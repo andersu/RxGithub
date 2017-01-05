7c64fccc59b49d132a2bf5d4a4bfa87625a069a0
Anders Ullnæss <andersullnass@gmail.com>
2017-01-05 23:09:56 +0100

    Added integration test for GitHubInteractor

---
+++ app/src/androidTest/java/no/zredna/rxgithub/interactor/GitHubInteractorIntegrationTest.java
@@ -0,0 +1,36 @@
+package no.zredna.rxgithub.interactor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import io.reactivex.Observable;
+import no.zredna.rxgithub.model.github.GitHubInformation;
+import no.zredna.rxgithub.service.GitHubServiceProvider;
+
+import static org.junit.Assert.*;
+
+public class GitHubInteractorIntegrationTest {
+    private GitHubInteractor gitHubInteractor;
+
+    @Before
+    public void setUp() {
+        gitHubInteractor = new GitHubInteractorImpl(new GitHubServiceProvider());
+    }
+    @Test
+    public void getGitHubInformation() throws Exception {
+        String username = "andersu";
+        String expectedName = "Anders Ullnæss";
+
+        Observable<GitHubInformation> observable = gitHubInteractor.getGitHubInformation(username);
+
+        GitHubInformation gitHubInformation = observable.blockingFirst();
+
+        int publicRepos = gitHubInformation.getUser().getPublicRepos();
+        assertEquals(username, gitHubInformation.getUser().getLogin());
+        assertEquals(expectedName, gitHubInformation.getUser().getName());
+        assertTrue(publicRepos > 0);
+        assertEquals(publicRepos, gitHubInformation.getRepos().size());
+        assertNotNull(gitHubInformation.getRepos().get(0).getName());
+        assertNotNull(gitHubInformation.getRepos().get(0).getCreatedAt());
+    }
+}
---
+++ app/src/test/java/no/zredna/rxgithub/RxGitHubTest.java
@@ -0,0 +1,12 @@
+package no.zredna.rxgithub;
+
+import org.junit.Before;
+import org.mockito.MockitoAnnotations;
+
+public class RxGitHubTest {
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+}
